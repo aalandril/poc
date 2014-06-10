68e0cd1fb65e3cb81409f9ec4f9add7187a1fc6f
aalandril <aalandril@gmail.com>
2014-06-10 13:13:14 +0100

    Create RandomNumberGenerator.java

---
+++ src/main/java/poc/RandomNumberGenerator.java
@@ -0,0 +1,15 @@
+package poc;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+	public int roll(RandomNumberConfigurer config) {
+		int total = 0;
+		Random randomGenerator = new Random();
+		for (int i = 0; i < config.getNumberOfRolls(); i++) {
+			int result = randomGenerator.nextInt(config.getTypeOfDice()) + 1;
+			total += result;
+		}
+		return total;
+	}
+}
