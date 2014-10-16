0d36d60657043ca8d223f97adce264e30fd43638
aalandril <aalandril@gmail.com>
2014-10-16 16:06:48 +0100

    Added button code

--- src/main/java/ui/swing/frame/CompanyNew.java
+++ src/main/java/ui/swing/frame/CompanyNew.java
@@ -1,15 +1,26 @@
 package ui.swing.frame;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class CompanyNew extends JInternalFrame {
+import poc.Company;
+import poc.DBManager;
+import poc.DBManagerImpl;
+import poc.swing.action.CompanyActions;
+
+public class CompanyNew extends JInternalFrame implements ActionListener {
 	private static final long serialVersionUID = 7025954823760323351L;
 	static final int xOffset = 30, yOffset = 30;
+	CompanyActions actions;
+	JTextField companyName;
+	DBManager manager = new DBManagerImpl();
 	
 	public CompanyNew() {
 		super("Document #CompanyNew",
@@ -17,6 +28,8 @@ public class CompanyNew extends JInternalFrame {
 				true, //closable
 				true, //maximizable
 				true); //iconifiable
+		
+		actions = new CompanyActions();
 		
 		// Create the GUI and put it in the window
 		
@@ -28,16 +41,30 @@ public class CompanyNew extends JInternalFrame {
 		
 		JPanel panel = new JPanel();
 		JLabel companyNameLabel = new JLabel("Company Name:");
-		JTextField companyName = new JTextField(20);
+		companyName = new JTextField(20);
+		JButton saveButton = new JButton("Save");
+		saveButton.addActionListener(this);
 		
 		this.setLayout(new BorderLayout());
 
 		panel.add(companyNameLabel);
 		panel.add(companyName);
+		panel.add(saveButton);
 
 		this.add(panel);
 		this.setVisible(true);
 		this.setSize(200,200);
 
+	}
+	
+	public void actionPerformed(ActionEvent e) { 
+		if ("Save".equals(e.getActionCommand())) { 
+			System.out.println("Save:" + companyName.getText());
+			Company company = new Company();
+			company.setName(companyName.getText());
+			manager.insertRecord(company, Company.class);
+		} else {
+			System.out.println("Else");
+		}
 	}
 }
